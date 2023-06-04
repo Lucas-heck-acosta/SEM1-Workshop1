be36b5cd8e787110a0424e14134bc8d6d358a08c
Lucas Acosta <70411977+Suco0@users.noreply.github.com>
2023-06-03 20:13:43 -0400

    First Commit

---
+++ w1p2.c
@@ -0,0 +1,39 @@
+/*
+*****************************************************************************
+Lucas Heck Acosta
+Workshop - #2 (Part-2)
+This a very simple workshop designed to teach first time programmers how to print text and simple formatting in C
+
+ Thanks for checking it
+*****************************************************************************
+*/
+
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Workshop 1 Part-2\n");
+	printf("=================\n");
+	printf("\n");
+	printf("Using tab specifiers...\n");
+	printf("\tTab-1\tTab-2\tTab-3\n");
+	printf("	-----	=====	-----\n");
+	printf("\n");
+	printf("\\ this is a back-slash character!\n");
+	printf("%% this is a percent sign character!\n");
+	printf("\" this is a double-quote character!\n");
+	printf("\n");
+	printf("My favourite quotes are:\n");
+	printf("1.	\"Skill is only developed by hours and hours of work.\"\n");
+	printf("						-Usain Bolt\n");
+	printf("\n");
+	printf("2.	\"It's not about having time. It's about making time.\"\n");
+	printf("						-unknown\n");
+	printf("\n");
+	printf("3.	\"All of us do not have equal talent. But, all of us\n");
+	printf("	have an equal opportunity to develop our talents.\"\n");
+	printf("						-A.P.J. Abdul Kalam\n");
+	
+	return 0;
+}
